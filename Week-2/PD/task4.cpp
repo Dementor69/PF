d21295d6c16dbb0706f7cc55cf5fc5bfbdb999e9
Dementor69 <120156842+Dementor69@users.noreply.github.com>
2023-02-11 15:55:24 +0500

    Add files via upload

---
+++ Week-2/PD/task4.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+system("Color 58");
+cout << "Demetor" <<endl;
+return 0;
+}
---
+++ Week-2/PD/task9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+cout<<"     o    ^    ^                                 "<<endl;
+cout<<"           ____                                  "<<endl;
+cout<<"       o  ( oo )\______________                  "<<endl;
+cout<<"                                                 "<<endl;
+cout<<"          (____)\              )\/\              "<<endl;
+cout<<"                ||~~~~~~~~~w   |                 "<<endl;
+cout<<"                ||            ||                 "<<endl;
+}
